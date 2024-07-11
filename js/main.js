cb256c2abf69694a639fd01f953e5f2cf37de77a
Alan Patsi <alanpatsi02@gmail.com>
2024-07-11 03:04:03 -0300

    Se agrega una función para validar la edad

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 const welcome = "Esta página es para mayores de 18 🚫, vamos a verificar tu edad 👮🏼‍♂️";
 alert(welcome);
+function validarEdad(edad) {
+    if (isNaN(edad)) {
+        return false;
+    }
+    edad = parseInt(edad);
+    if (edad >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
